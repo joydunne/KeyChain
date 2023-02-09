8efea0e4c4dbc893f78b01daadae1bf7526021a1
joydunne <joydunne33@gmail.com>
2023-02-09 13:21:46 -0600

    updated php

--- text.php
+++ text.php
@@ -1,4 +1,14 @@
 <?php
-    echo "Hi Brooke, Why are you late???";
-    echo "and you too Coen!!!";
+    echo "Hi <em>Brooke<em>, Why are you late???";
+    echo "and you too Coen!!!<br>";
+    print_r ($_GET) . "<br>";
+    echo "Hello" . $_GET ["name"] . "<p>";
+
+    foreach ($_GET as $id => $val){
+        echo $id . "==>" . $val . "<br>";}
+
+    $f = fopen ("test.json" , "W");
+    fwrite ($f, "This is a file.");
+    fclose($f);
+    echo "saved";
 ?>
