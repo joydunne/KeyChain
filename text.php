c72cca4e13caa5ac57b7189fec404a040288b866
joydunne <joydunne33@gmail.com>
2023-03-02 13:31:52 -0600

    ,

--- text.php
+++ text.php
@@ -7,12 +7,12 @@
     foreach ($_GET as $id => $val){
         echo $id . "==>" . $val . "<br>";}
 
-    echo '<button type = "button"> Input!</button>'
-    $f = fopen ("test.json" , "a"); #w means if that file exist already it delets the old and puts the new one. a means it adds on to it 
-    fwrite($f, "This is a file.\n");
-    fwrite($f, json_encode($_GET) . "\n");
-    fclose($f);
-    echo "Saved";
+    // echo '<button type = "button"> Input!</button>'
+    // $f = fopen ("test.json" , "a"); #w means if that file exist already it delets the old and puts the new one. a means it adds on to it 
+    // fwrite($f, "This is a file.\n");
+    // fwrite($f, json_encode($_GET) . "\n");
+    // fclose($f);
+    // echo "Saved";
 
        
 ?>
